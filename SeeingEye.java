086b9d139cd241fd8c669df95065c107201f7fbe
Vincent-Acocella <58150749+Vincent-Acocella@users.noreply.github.com>
2020-02-24 20:54:27 -0500

    Add files via upload

---
+++ SeeingEye.java
@@ -0,0 +1,55 @@
+
+public class SeeingEye {
+	
+	float kp;
+	float kd;
+	float ki;
+	
+	public static void main(String[] args){
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+	//Correction is proportional to error using direction and magnitude
+	//Take in sensor data and compare it to the distance from the wall that we want. 
+	//Change motor speed to compensate
+	//kp (gain) is the multiplier of the speed that the change happens 
+	public static float returnProportional(float kp)
+	{
+		
+		
+	}
+	
+	
+	//Produces output that is proportional to the
+	//derivative of the input
+	public static float returnDiriv(float kd)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	//System tracks errors, sums them up and once the
+	//total error crosses a threshold, the system corrects
+	public static float returnSumation(float ki)
+	{
+		
+	}
+	
+}
+
+
+
+
